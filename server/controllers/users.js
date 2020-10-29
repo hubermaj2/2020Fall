5c439c25f4cc96aa63d2fcd75f8c6b6d5767ac89
hubermaj2 <hubermaj2@hawkmail.newpaltz.edu>
2020-10-29 14:40:32 -0400

    10-29

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -7,22 +7,18 @@ const users = require('../models/users');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/', (req, res, next) => {
         users.getAll().then(x=> res.send( x ) )
         .catch(next);
     })
-    .get('/search', (req, res) => {
+    .get('/search', (req, res, next) => {
         users.search(req.query.q).then(x=> res.send( x ) )
         .catch(next);
     })
-    .post('/', (req, res) => {
+    .post('/', (req, res, next) => {
         users.add(req.query.name, req.query.age ).then(newUser => {
             res.send( newUser );
         }).catch(next)
-    })
-    .get('/rand', (req, res, next) => {
-        const someVal = users.rand();
-        res.send({someVal});
     })
 
 module.exports = router;
--- server/models/users.js
+++ server/models/users.js
@@ -2,32 +2,20 @@
 
 */
 
+const mysql = require('./mysql');
+
 const data = [{ name: 'Moshe', age: 43}, { name: 'Biden', age: 78 }]
 
 async function getAll(){
     //throw { status: 501, message: "This is a fake error" }
     //await Promise.resolve()
     console.log("Called Get All")
-    return data;
+    return mysql.query('SELECT * FROM Users');
 }
 
 async function add(name, age){
     data.push({name, age});
 }
 
-function rand() {
-    var someVal = 0;
-    const p = new Promise((resolve, reject)=>{
 
-    })
-    setTimeout(() => {
-        for (let index = 0; index < 999*999; index++){
-            someVal = index * Math.random();
-        }
-        resolve(someVal)
-    }, 1);
-    return;
-}
-
-
-module.exports = { getAll, rand, add, search: async q => data.filter(x=> x.name == q) }
+module.exports = { getAll, add, search: async q => data.filter(x=> x.name == q) }
